c9e6826b1214b75a45fa9685652b104c10eaded5
Joshua <lyu6@stolaf.edu>
2020-04-30 20:42:14 -0500

    b.cpp

--- a.cpp
+++ a.cpp
@@ -1,4 +1,4 @@
-#include"../react.h"
+#include"react.h"
 
 int main() {
   init();
@@ -10,16 +10,19 @@ int main() {
   print_at(50, "order history");
   print_at(70, "favorites");
   print_at(100, "Item                Price               Seller");
-  print_at(130, "Post");
+  print_at(130, "Post"); 
   print_at(200, "history");
   print_at(210, "likes");
   print_at(310, "Item       Description");
   print_at(240, "Name");
   print_at(250, "Description");
-  print_at(260, "Contact Info");
+  print_at(262, "Contact Info");
   print_at(275, "My Posts");
   print_at(290, "My Orders");
   print_at(300, "Profile");
+  print_at(310, "<< ");
+
+
   
 
   char state = get_char_at(2);
@@ -31,13 +34,25 @@ int main() {
       state = '1';
       put_char_at(2, state);
   } 
+    else if (event_id_is("postpage")) {
+      state = '2';
+      put_char_at(2, state);
+    }
+    else if (event_id_is("profilepage")) {
+      state = '3';
+      put_char_at(2, state);
+    }
+    else if (event_id_is("order_history_page")) {
+      state = '4';
+      put_char_at(2, state);
+    }
   }
 
   if ('0' == state) add_yaml("login_page.yaml");
   else if('1' == state) add_yaml("main_page.yaml");
   else if('2' == state) add_yaml("post_page.yaml");
-  else if('3' == state) add_yaml("likes_page.yaml");
-  else if('4'== state) add_yaml("profile_page.yaml");  
+  else if('3'== state) add_yaml("profile_page.yaml");  
+  else if('4' == state) add_yaml("history_page.yaml");
 
   quit();
 }
