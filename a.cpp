e7030ec7b8dc9c14d4fb7fa5c0fd36b554739a23
Joshua <lyu6@stolaf.edu>
2020-05-15 16:27:46 -0500

    recent

--- a.cpp
+++ a.cpp
@@ -199,38 +199,7 @@ class User:public Login {
     }
 void delete_line() 
 { 
-    // open file in read mode or in mode 
-    ifstream is(file_name); 
-  
-    // open file in write mode or out mode 
-    ofstream ofs; 
-    ofs.open("temp.txt", ofstream::out); 
-  
-    // loop getting single characters 
-    char c; 
-    int line_no = 1; 
-    while (is.get(c)) 
-    { 
-        // if a newline character 
-        if (c == '\n') 
-        line_no++; 
-  
-        // file content not to be deleted 
-        if (line_no != n) 
-            ofs << c; 
-    } 
-  
-    // closing output file 
-    ofs.close(); 
-  
-    // closing input file 
-    is.close(); 
-  
-    // remove the original file 
-    remove(file_name); 
-  
-    // rename the file 
-    rename("temp.txt", file_name); 
+
 } 
   
 
